c50c4e4225f3bb649feac0e67aaa9033ed5c857c
Mikail Rokib <atandarokib@gmail.com>
2023-05-29 13:24:03 +0100

    updated

--- src/Components/Footer.jsx
+++ src/Components/Footer.jsx
@@ -3,7 +3,8 @@ import React from "react";
 const Footer = () => {
   return (
     <footer className="py-[20px] w-full bg-black text-white">
-      <div className="container flex justify-center items-center font-Karasu">
+      <div className="container flex-col gap-4 flex justify-center items-center font-Karasu">
+        <p>yugifouga@gmail.com</p>
         <p>Copyright © 2023 made in japan</p>
       </div>
     </footer>
